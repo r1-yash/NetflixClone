e318d7b80f1b0143fbab47a325bb446ece5a1efa
r1-yash <yashsinghalxd@gmail.com>
2025-12-30 22:06:36 +0530

    Home changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Home.css' 
 import NavBar from '../../components/NavBar/NavBar'
-
-
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
 function Home() {
   return (
     <div className='home'>
         <NavBar/>
+        <div className="hero">
+          <img src={hero_banner} className='banner-img'></img>
+          <div className="hero-caption">
+            <img src={hero_title} className='caption-img'></img>
+            <p>Hakan is a young shopkeeper whose modern world gets turned upside down when he learns that he is connected to a secret ancient order that is tasked with protecting Istanbul from an immortal enemy.</p>
+          </div>
+        </div>
     </div>
   )
 }
