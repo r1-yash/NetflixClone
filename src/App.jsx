cacded46ea1b352b1f94e6aa1ae2ea6d34fd50e7
r1-yash <yashsinghalxd@gmail.com>
2025-12-30 16:06:23 +0530

    Navbar Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import Home from './pages/Home/Home'
-import './App.css'
 
 function App() {
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Navbar.css'
+import './NavBar.css'
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg'
 import bell_icon from '../../assets/bell_icon.svg'
@@ -11,8 +11,8 @@ function NavBar() {
     <div className='navbar'>
         <div className="navbar-left" >
             {/* the ul is important as it remove the styling or the bullets */}
+            <img src={logo}></img>
             <ul> 
-                <img src={logo}></img>
                 <li>Home</li>
                 <li>TV Shows</li>
                  <li>Movies</li>
@@ -21,13 +21,14 @@ function NavBar() {
                 <li>Browse by Language</li>
             </ul>
         </div>
-        <div className="navbar-right"></div>
-        <img src={search_icon} className='icons'></img>
-        <p>Children</p>
-        <img src={bell_icon} className='icons'></img>
-        <div className='navbar-profile'>
-          <img src={profile_img} className='profile'></img>
-          <img src={caret_icon} className='profile'></img>
+        <div className="navbar-right">
+          <img src={search_icon} className='icons'></img>
+          <p>Children</p>
+          <img src={bell_icon} className='icons'></img>
+          <div className='navbar-profile'>
+            <img src={profile_img} className='profile'></img>
+            <img src={caret_icon} className='profile'></img>
+          </div>
         </div>
     </div>
   )
