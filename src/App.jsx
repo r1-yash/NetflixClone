c06a33564c7e6d5bef0ddd74049837e70a2e74a4
r1-yash <yashsinghalxd@gmail.com>
2026-01-01 23:50:58 +0530

    Login page work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import Home from './pages/Home/Home'
-
+import {Routes, Route} from 'react-router-dom'
+import Login from '../src/pages/Login/Login'
 function App() {
 
   return (
     <>
-      <Home/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
+      
     </>
   )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
+
 function Login() {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+       <img src={logo} className='login-logo'></img>
+      <form className='login-form'>
+        <input type='text' placeholder='Your name'></input>
+        <input type='email' placeholder='Email'></input>
+        <input type='password' placeholder='Password'></input>
+        <button>Sign In</button>
+      </form>
+    </div>
   )
 }
 
