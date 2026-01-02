75199e2fccbe26a09eae1e05b350d1dd8e5c7d32
r1-yash <yashsinghalxd@gmail.com>
2026-01-02 21:47:02 +0530

    Login Page complete

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 
 function Login() {
+  const [signState, setSignState] = useState("Sign In")
+
   return (
     <div className='login'>
-       <img src={logo} className='login-logo'></img>
+      <div>
+      <img src={logo} className='login-logo'></img>
       <form className='login-form'>
-        <h1>Sign up</h1>
-        <input type='text' placeholder='Your name'></input>
+        <h1>{signState}</h1>
+        {signState==="Sign Up"?<input type='text' placeholder='Your name'></input>:<></>
+}
         <input type='email' placeholder='Email'></input>
         <input type='password' placeholder='Password'></input>
-        <button>Sign up</button>
+        <button>{signState}</button>
         <div className='form-help'>
           <div className='remember'>
           <input type='checkbox'></input>
@@ -19,9 +23,13 @@ function Login() {
           </div>
           <p>Need Help?</p>
         </div>
+        <div className='form-switch'>
+          {signState === "Sign In" ?<p>New to Netflix ? <span onClick={() => {
+            setSignState("Sign Up")
+          }}>Sign Up Now</span></p>:<p>Already have account? <span>Sign In Now</span></p>
+}
+      </div>
       </form>
-      <div className='form-switch'>
-
       </div>
     </div>
   )
